be3b031d63ccd2e41de15726a2b041985bfc05ca
philm <philmayfield@gmail.com>
2018-08-10 12:19:06 -0700

    change totalplaces comonent to be a fragment

--- client/src/components/places/TotalPlaces.js
+++ client/src/components/places/TotalPlaces.js
@@ -1,16 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
-const TotalPlaces = ({ number = 0, classes = "" }) => {
-  return (
-    <div className={`total-places ${classes}`}>
-      {number === 0 ? "No " : `${number} `}
-      place
-      {number > 1 || number === 0 ? "s " : " "}
-      on the list
-    </div>
-  );
-};
+const TotalPlaces = ({ number = 0 }) => (
+  <Fragment>
+    {number === 0 ? "No " : `${number} `}
+    place
+    {number > 1 || number === 0 ? "s " : " "}
+    on the list
+  </Fragment>
+);
 
 TotalPlaces.propTypes = {
   number: PropTypes.number,

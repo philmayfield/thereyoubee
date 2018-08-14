03208f9e3fd4845e1be1c4852464ddd684f10c2e
philm <philmayfield@gmail.com>
2018-08-14 15:19:34 -0700

    added animation when adding / removing items in list view

--- client/src/components/list/ListItem.js
+++ client/src/components/list/ListItem.js
@@ -3,18 +3,27 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Button from "../common/Button";
 import { flagPlace } from "../../actions/placeActions";
+import { CSSTransition } from "react-transition-group";
 
 class ListItem extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.animationTime = 250;
+    this.state = {
+      show: true
+    };
 
     this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleDelete() {
+    this.setState({ show: false });
     const { item, flagPlace } = this.props;
-    flagPlace(item);
+
+    // give the animation time to finish
+    setTimeout(() => {
+      flagPlace(item);
+    }, this.animationTime);
   }
 
   render() {
@@ -29,36 +38,45 @@ class ListItem extends Component {
     // const { lat, lng } = latLng;
 
     return (
-      <div className="card">
-        <div className="card-title-area with-fab teal">
-          <span className="card-title white-text">{suggestion}</span>
-          <Button
-            icon="menu"
-            fab={true}
-            classes={[
-              "btn-floating",
-              // "btn-large",
-              "halfway-fab",
-              "teal",
-              "lighten-2"
-            ]}
-          >
+      <CSSTransition
+        in={this.state.show}
+        appear={true}
+        timeout={0}
+        classNames="place-card"
+      >
+        <div className="card place-card">
+          <div className="card-title-area with-fab teal">
+            <i className="material-icons mr-2 place-icon white-text">place</i>
+            <span className="card-title white-text">{suggestion}</span>
             <Button
-              icon="delete_forever"
-              classes={["btn-floating", "red"]}
-              clickOrTo={this.handleDelete}
-            />
-          </Button>
+              icon="menu"
+              fab={true}
+              classes={[
+                "btn-floating",
+                // "btn-large",
+                "halfway-fab",
+                "teal",
+                "lighten-2"
+              ]}
+            >
+              <Button
+                icon="delete_forever"
+                classes={["btn-floating", "red"]}
+                clickOrTo={this.handleDelete}
+              />
+            </Button>
+          </div>
+          <div className="card-content">
+            <p>{address}</p>
+          </div>
         </div>
-        <div className="card-content">
-          <p>{address}</p>
-        </div>
-      </div>
+      </CSSTransition>
     );
   }
 }
 
 ListItem.propTypes = {
+  index: PropTypes.number,
   flagPlace: PropTypes.func.isRequired,
   item: PropTypes.object
 };

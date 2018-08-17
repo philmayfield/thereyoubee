9bf332c000c196a449ecf768b9d4dc15c626abbf
philm <philmayfield@gmail.com>
2018-08-17 15:17:22 -0700

    setup list reducers and added functionality around lists

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,12 +5,16 @@ import currentPlaceReducer from "./currentPlaceReducer";
 import errorReducer from "./errorReducer";
 import placesReducer from "./placesReducer";
 import toastReducer from "./toastReducer";
+import currentListReducer from "./currentListReducer";
+import listsReducer from "./listsReducer";
 
 export default combineReducers({
   app: appReducer,
   auth: authReducer,
   currentPlace: currentPlaceReducer,
+  currentList: currentListReducer,
   errors: errorReducer,
   places: placesReducer,
+  lists: listsReducer,
   toast: toastReducer
 });

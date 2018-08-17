32d623a6a6be7c4038d61d2615f43a32522cb4eb
philm <philmayfield@gmail.com>
2018-08-17 13:19:56 -0700

    setup for list functionality

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -11,14 +11,20 @@ export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
 // places
-export const FLAG_PLACE = "FLAG_PLACE";
-export const UNFLAG_PLACE = "UNFLAG_PLACE";
 export const GET_PLACES = "GET_PLACES";
 export const SET_CURRENT_PLACE = "SET_CURRENT_PLACE";
 export const SAVE_CURRENT_PLACE = "SAVE_CURRENT_PLACE";
-export const RESET_CURRENT_PLACE = "RESET_CURRENT_PLACE";
 export const DELETE_PLACE = "DELETE_PLACE";
+export const FLAG_PLACE = "FLAG_PLACE";
+export const UNFLAG_PLACE = "UNFLAG_PLACE";
 export const RESET_PLACES = "RESET_PLACES";
+export const RESET_CURRENT_PLACE = "RESET_CURRENT_PLACE";
+
+// lists
+export const GET_LISTS = "GET_LISTS";
+export const SET_LIST = "SET_LIST";
+export const SAVE_LIST = "SAVE_LIST";
+export const DELETE_LIST = "DELETE_LIST";
 
 // toast
 export const ADD_TOAST = "ADD_TOAST";
---
+++ client/src/actions/listActions.js
@@ -0,0 +1,93 @@
+import axios from "axios";
+import { GET_LISTS, SET_LIST, SAVE_LIST, DELETE_LIST } from "./actionTypes";
+import { getErrors, clearErrors, isLoading, notLoading } from "./appActions";
+import { addToast } from "./toastActions";
+
+// get all of the saved lists
+export const getAllLists = () => dispatch => {
+  dispatch(isLoading("getAllLists"));
+
+  axios
+    .get("/api/list/all")
+    .then(res => {
+      dispatch({
+        type: GET_LISTS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      const { response } = err;
+      const error = response
+        ? {
+            status: response.status,
+            data: response.data
+          }
+        : {
+            status: "",
+            data: err
+          };
+
+      dispatch(getErrors(error));
+
+      if (response && response.status === 500) {
+        dispatch(
+          addToast({
+            value: `Uh oh, thats a 500 response, try refreshing.`,
+            icon: "error",
+            showClose: true
+          })
+        );
+      }
+    })
+    .finally(() => dispatch(notLoading("getAllLists")));
+};
+
+// delete a list from the store and database
+export const deleteList = list => dispatch => {
+  axios
+    .delete(`/api/list/${list._id}`)
+    .then(() => {
+      dispatch({
+        type: DELETE_LIST,
+        payload: list._id
+      });
+    })
+    .catch(err => {
+      const error = err.response ? err.response.data : err;
+      dispatch(getErrors(error));
+    });
+};
+
+// save the current list to the database
+export const saveCurrentList = list => dispatch => {
+  dispatch(clearErrors());
+  dispatch(isLoading("saveCurrentList"));
+
+  axios
+    .post("/api/list/", list)
+    .then(payload => {
+      dispatch({
+        payload,
+        type: SAVE_LIST
+      });
+      dispatch(
+        addToast({
+          value: `Added ${list.name}`,
+          icon: "thumb_up"
+        })
+      );
+    })
+    .catch(err => {
+      const error = err.response ? err.response.data : err;
+      dispatch(getErrors(error));
+    })
+    .finally(() => dispatch(notLoading("saveCurrentList")));
+};
+
+// set the current list on the app
+export const setList = payload => {
+  return {
+    payload,
+    type: SET_LIST
+  };
+};

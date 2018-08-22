85030b78fb707b577e98c423fd80552985001d2a
philm <philmayfield@gmail.com>
2018-08-21 17:07:43 -0700

    prep total places to eventually show list name

--- client/src/common/totalPlaces.js
+++ client/src/common/totalPlaces.js
@@ -1,9 +1,9 @@
-const totalPlaces = (number = 0, loading = false) => {
+const totalPlaces = (number = 0, loading = false, listName = "on the list") => {
   return loading
     ? "Fetching places list…"
     : `${number === 0 ? "No" : number} place${
         number > 1 || number === 0 ? "s" : ""
-      } on the list`;
+      } ${listName}`;
 };
 
 export default totalPlaces;
